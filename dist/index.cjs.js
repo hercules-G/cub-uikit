2c25ee47bde4b1a8b93958b2b3e47e316f4b082c
hercules-G <zeusgwarrior@gmail.com>
2023-10-10 16:32:20 +0530

    Revert "int"
    This reverts commit 0377ede7a5aa0474044267567d84e8194aadd7cf.

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3370,7 +3370,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/financehercules",
+        href: "https://twitter.com/financeleo",
     },
 ];
 var MENU_HEIGHT = 64;
